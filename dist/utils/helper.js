068f902bd646dd6c9293d330fad0fdb39ebd1f1e
Dmitry-lasasimma <dim.lasasimma.12@gmail.com>
2025-01-19 14:34:43 +0700

    init project 03

--- dist/utils/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUserId = void 0;
-const generateUserId = () => {
-    return Math.floor(10000000 + Math.random() * 90000000).toString();
-};
-exports.generateUserId = generateUserId;
